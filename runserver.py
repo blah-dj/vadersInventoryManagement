63d74b34f81c86f476f19c7a838324043396ea03
Ubuntu <vaders@VadersAppDevelopmentVM.xvntebwtvllejabvnrdnlkow1b.bx.internal.cloudapp.net>
2017-02-27 04:35:46 +0000

    removed FlaskWebProject1

--- runserver.py
+++ runserver.py
@@ -1,14 +1,15 @@
 """
-This script runs the FlaskWebProject1 application using a development server.
+This script runs the InventoryManagement application using a development server.
 """
 
 from os import environ
-from FlaskWebProject1 import app
+from InventoryManagement import app
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(environ.get('SERVER_PORT', '80'))
     except ValueError:
-        PORT = 5555
+        PORT = 80
     app.run(HOST, PORT)
+
